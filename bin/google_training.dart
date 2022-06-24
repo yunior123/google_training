4e33a12735bb54d17eec492526145bac8a7b83dc
yunior123 <yuniorrodriguezo460@gmail.com>
2022-06-24 09:05:01 -0400

    Initial commit

---
+++ bin/google_training.dart
@@ -0,0 +1,17 @@
+import 'package:google_training/google_training.dart' as google_training;
+
+void main(List<String> arguments) {
+  print('Hello world: ${google_training.calculate()}!');
+}
+
+// Find the number of ways that a given integer, , can be expressed as the sum of the  powers of unique, natural numbers.
+// For example, if  and , we have to find all combinations of unique squares adding up to . The only solution is  .
+// Function Description
+// Complete the powerSum function in the editor below. It should return an integer that represents the number of possible combinations.
+// powerSum has the following parameter(s):
+// X: the integer to sum to
+// N: the integer power to raise numbers to
+
+int powerSum(int x, int n) {
+  // Write your code here
+}
---
+++ test/google_training_test.dart
@@ -0,0 +1,8 @@
+import 'package:google_training/google_training.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
